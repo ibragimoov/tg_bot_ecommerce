33e3bc29ff4b1e3cb91a3ab0be9b101ce3b79f3e
Ibragimov Eldar <1ibragimov.e.i20@gmail.com>
2024-12-15 12:24:57 +0300

    push

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ from app.database.models import async_main
 
 
 async def main():
-    await async_main()
-    bot = Bot(token='')
+    bot = Bot(token='8101609114:AAGEMmGwCm3m-SQ70jYQwVtayd2N8Wk0qBY')
     dp = Dispatcher()
     dp.include_router(router)
+    await async_main()
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
+    print('Бот запущен')
     asyncio.run(main())
+
+#1 вспомнить код
+#2 узнать, что такое бд
+#3 как использовать бд + питон
+#4 практика
